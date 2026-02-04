bc0a7ce18a0ef8e0f51368c2ebc5e56e52159e56
Oliver Smith <osmith@netvirta.com>
2026-02-04 14:57:15 +0000

    Disable OTA recovery updates

--- device.mk
+++ device.mk
@@ -418,9 +418,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/configs/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
 
-# Recovery
-PRODUCT_PACKAGES += \
-    librecovery_updater_qcom
+# Recovery - Disable
+#PRODUCT_PACKAGES += \
+#    librecovery_updater_qcom
 
 # Ril
 PRODUCT_PACKAGES += \
