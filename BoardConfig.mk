3166a00ea24329569656f443a64a80895cccb4a5
Datty <datty.wtb@gmail.com>
2026-02-06 11:14:29 +0000

    Initial changes to remove old references to -TARGET_INIT_VENDOR_LIB and -TARGET_SURFACEFLINGER_UDFPS_LIB. Improvements to fingerprint reader stability
    Change-Id: Iddd214100194700d261d42e883fc584cc3bb9932

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,8 +98,15 @@ TARGET_HAS_WIDE_COLOR_DISPLAY := true
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # FOD
-TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.samurai
+SOONG_CONFIG_NAMESPACES += surfaceflinger
 TARGET_USES_FOD_ZPOS := true
+#Added to fix new naming
+SOONG_CONFIG_surfaceflinger += udfps_lib
+SOONG_CONFIG_surfaceflinger_udfps_lib := libudfps_extension.samurai
+#Added by suggestion for zlayer issues
+SOONG_CONFIG_surfaceflinger_udfps_dim_layer := false
+TARGET_HAS_OPTICAL_FOD := true
+TARGET_USES_FOD_BRIGHTNESS := true
 
 # Hidl
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
@@ -110,7 +117,9 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_samurai
+SOONG_CONFIG_NAMESPACES += libinit
+SOONG_CONFIG_libinit += vendor_init_lib
+SOONG_CONFIG_libinit_vendor_init_lib := libinit_samurai
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
