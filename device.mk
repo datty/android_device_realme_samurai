0c918bb5c44a6f01c6664929cb35b08263c09a89
Datty <datty.wtb@gmail.com>
2026-02-06 14:09:07 +0000

    shift NfcNci package to vendor, not allowed to build as standard product package
    Change-Id: I2fd1f3652aa19875aa49c783808fd664dd48f427

--- device.mk
+++ device.mk
@@ -333,10 +333,12 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \
     android.hardware.nfc@1.2.vendor \
     com.android.nfc_extras \
-    NfcNci \
     nqnfcinfo \
     SecureElement \
     Tag
+
+PRODUCT_PACKAGES_VENDOR += \
+    NfcNci
 
 # OMX
 PRODUCT_PACKAGES += \
